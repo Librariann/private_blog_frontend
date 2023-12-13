acafa23dbc44d4e89aa441decee048b6c835533a
Librariann <okpc0306@naver.com>
2023-12-13 15:10:03 +0900

    [commit, delete] add apolloClient Setting in _app.tsx, delete codegen.yml

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,25 @@
 import type { AppProps } from "next/app";
 import Layout from "../components/layout";
-import { ApolloProvider } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
 import "../styles/globals.css";
 
-const client = "";
+//TODO: product 배포시 사용 할 예정
+// const httpLink = createHttpLink({
+//   uri:
+//     process.env.NODE_ENV === "production"
+//       ? ""
+//       : "http://localhost:3000/graphql",
+// });
+
+const client = new ApolloClient({
+  uri: "http://localhost:3000/graphql",
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
