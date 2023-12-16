35d629e2d2cbb355d9d4af5a6f4bcacdfc7a8272
Librariann <okpc0306@naver.com>
2023-12-16 23:30:18 +0900

    [commit] adding.. jwtToken settings..

--- apollo.ts
+++ apollo.ts
@@ -1,14 +1,18 @@
 //TODO: product 배포시 사용 할 예정
 import { InMemoryCache, ApolloClient, makeVar } from "@apollo/client";
+import { LOCAL_STORAGE_TOKEN } from "./common/constants";
+import { useEffect } from "react";
 // const httpLink = createHttpLink({
 //   uri:
 //     process.env.NODE_ENV === "production"
 //       ? ""
 //       : "http://localhost:3000/graphql",
 // });
-
-const token = localStorage.getItem(LOCAL_STORAGE_TOKEN);
-export const authTokenVar = makeVar(token);
+export let authTokenVar;
+if (typeof window !== "undefined") {
+  const token = localStorage.getItem(LOCAL_STORAGE_TOKEN);
+  authTokenVar = makeVar(token);
+}
 
 export const client = new ApolloClient({
   uri: "http://localhost:3000/graphql",
--- common/constants.ts
+++ common/constants.ts
@@ -1,1 +1,1 @@
-const LOCAL_STORAGE_TOKEN = "token";
+export const LOCAL_STORAGE_TOKEN = "token";
