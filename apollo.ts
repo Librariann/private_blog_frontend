efd2a8ba14232781b1d4a43a1d098827af00c37a
Librariann <okpc0306@naver.com>
2024-02-24 10:00:00 +0900

    [commit] add apollo.ts use client

--- apollo.ts
+++ apollo.ts
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   InMemoryCache,
   ApolloClient,
@@ -7,9 +9,14 @@ import {
 import { setContext } from "@apollo/client/link/context";
 import { LOCAL_STORAGE_TOKEN } from "./common/constants";
 
-export const isBrowser = typeof window !== "undefined";
-const token = isBrowser ? localStorage.getItem(LOCAL_STORAGE_TOKEN) : null;
-console.log(token);
+// 클라이언트에서만 사용할 수 있는 localStorage 접근
+const getAuthToken = () => {
+  if (typeof window !== "undefined") {
+    return localStorage.getItem(LOCAL_STORAGE_TOKEN);
+  }
+  return null;
+};
+const token = getAuthToken();
 export const authTokenVar = makeVar(token);
 export const isLoggedInVar = makeVar(Boolean(token));
 
