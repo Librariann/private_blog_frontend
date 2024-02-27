bde6e9659de558dcf2aa437e18433051f9654827
Librariann <okpc0306@naver.com>
2024-02-27 10:00:00 +0900

    [commit] modified _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,9 @@ import { client, isLoggedInVar } from "@/apollo";
 import { ApolloProvider, useReactiveVar } from "@apollo/client";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const isLoggedIn = useReactiveVar(isLoggedInVar);
   return (
     <ApolloProvider client={client}>
       <Layout>
-        {isLoggedIn ? "login" : "logout"}
         <Component {...pageProps} />
       </Layout>
     </ApolloProvider>
