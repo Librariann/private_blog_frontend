ceb919a0f04e12ee9ed5216a672526a43a6b40b1
Librariann <okpc0306@naver.com>
2024-03-07 10:00:00 +0900

    [commit] edit codegen.ts

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "http://localhost:3000/graphql",
+  schema: "http://localhost:3003/graphql",
   documents: "pages/**/*.tsx",
   generates: {
     "pages/gql/": {
