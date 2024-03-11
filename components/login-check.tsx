1ca6687adf79cc035f48416014cb35630ed89e77
Librariann <okpc0306@naver.com>
2024-03-11 10:00:00 +0900

    [commit] add login-check.tsx

---
+++ components/login-check.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { LOCAL_STORAGE_TOKEN } from "@/common/constants";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+const LoginCheck = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem(LOCAL_STORAGE_TOKEN);
+    const currentPageUri = router.asPath;
+    if (!token) {
+      router.push("/login");
+    }
+
+    if (
+      token &&
+      (currentPageUri === "/login" || currentPageUri === "/create-account")
+    ) {
+      router.push("/");
+    }
+  }, []);
+
+  return null;
+};
+
+export default LoginCheck;
