0c1cdd97eb3cdfaa080330f3f3f7876fa4c1c5d0
Librariann <okpc0306@naver.com>
2024-03-13 10:00:00 +0900

    [commit] edit index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,17 @@
 import { authTokenVar, isLoggedInVar } from "@/apollo";
 import { LOCAL_STORAGE_TOKEN } from "@/common/constants";
 import UserInfo from "@/components/user-info";
-import { useMe } from "@/hooks/useMe";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
   const logout = () => {
     localStorage.setItem(LOCAL_STORAGE_TOKEN, "");
+    router.push("/login");
+  };
+
+  const routerTest = () => {
+    router.push("/TEST");
   };
 
   return (
@@ -15,6 +21,12 @@ export default function Home() {
       <div className="">Hello World!</div>
       <UserInfo />
       <button onClick={logout}>로그아웃</button>
+      <span
+        className="cursor-pointer font-bold text-xl underline decoration-sky-500 hover:text-red-600"
+        onClick={routerTest}
+      >
+        여기
+      </span>
     </>
   );
 }
