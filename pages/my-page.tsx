329519a3ea6e8f145613934a887ad6167483e157
SeongHyun Park <librarian@SeongHyunui-MacBookAir.local>
2024-03-30 10:00:00 +0900

    [commit] edit my-page.tsx

--- pages/my-page.tsx
+++ pages/my-page.tsx
@@ -1,24 +1,28 @@
 import Button from "@/components/button";
 import Input from "@/components/input";
 import UserInfo from "@/components/user-info";
-import { gql } from "@apollo/client";
+import { gql, useMutation } from "@apollo/client";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import {
+  ChangePasswordMutation,
+  ChangePasswordMutationVariables,
+} from "./gql/graphql";
 
 interface IChangePasswordForm {
   password: string;
   checkPassword: string;
 }
 
-// export const CHANGE_PASSWORD_MUTATION = gql`
-//   mutation changePassword(password: string) {
-//     changePassword(password: string) {
-//       ok
-//       message
-//     }
-//   }
-// `;
+export const CHANGE_PASSWORD_MUTATION = gql`
+  mutation changePassword($password: String!) {
+    changePassword(password: $password) {
+      ok
+      message
+    }
+  }
+`;
 
 const MyPage = () => {
   const [password, setPassword] = useState<string>();
@@ -33,55 +37,58 @@ const MyPage = () => {
     mode: "onChange",
   });
 
+  const [changePasswordMutation] = useMutation<
+    ChangePasswordMutation,
+    ChangePasswordMutationVariables
+  >(CHANGE_PASSWORD_MUTATION);
+
   const router = useRouter();
 
   const routingMainPage = () => {
     router.push("/");
   };
 
-  const statePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
-  };
-
-  const stateCheckPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCheckPassword(e.target.value);
-  };
-
-  const onSubmit = () => {
+  const onSubmit = async () => {
     const { password, checkPassword } = getValues();
-    console.log(password, checkPassword);
-    // CHANGE_PASSWORD_MUTATION({
-    //   variables: {
-    //     loginInput: { email, password },
-    //   },
-    // });
+    if (password === checkPassword) {
+      const result = await changePasswordMutation({
+        variables: {
+          password,
+        },
+      });
+      alert(result.data?.changePassword.message);
+    } else {
+      alert("비밀번호가 다릅니다.");
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <>
       <UserInfo />
-      <ul>
-        <li>비밀번호 변경:</li>
-        <li>
-          <input
-            {...register("password")}
-            type="password"
-            className="password"
-            onChange={statePassword}
-          />
-        </li>
-        <li>
-          <input
-            {...register("checkPassword")}
-            type="password"
-            className="checkPassword"
-            onChange={stateCheckPassword}
-          />
-        </li>
-      </ul>
-      <Button canClick={isValid} loading={false} actionText="변경"></Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <ul>
+          <li>비밀번호 변경:</li>
+          <li>
+            <input
+              {...register("password")}
+              type="password"
+              className="password"
+              // onChange={statePassword}
+            />
+          </li>
+          <li>
+            <input
+              {...register("checkPassword")}
+              type="password"
+              className="checkPassword"
+              // onChange={stateCheckPassword}
+            />
+          </li>
+        </ul>
+        <Button canClick={isValid} loading={false} actionText="변경"></Button>
+      </form>
       <button onClick={routingMainPage}>메인으로</button>
-    </form>
+    </>
   );
 };
 
