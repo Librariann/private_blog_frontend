9e09d68032d9c85f3e787cea1ec621969273b0d8
Librariann <okpc0306@naver.com>
2024-04-18 10:00:10 +0900

    [commit] edit _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,19 @@
 import type { AppProps } from "next/app";
 import Layout from "../components/layout";
 import "../styles/globals.css";
-import { client, isLoggedInVar } from "@/apollo";
-import { ApolloProvider, useReactiveVar } from "@apollo/client";
+import { client } from "@/apollo";
+import { ApolloProvider } from "@apollo/client";
 import LoginCheck from "../components/login-check";
+import Header from "@/components/header";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const navigate = useRouter();
   return (
     <ApolloProvider client={client}>
       <Layout>
-        <LoginCheck />
+        {/* <LoginCheck */}
+        <Header />
         <Component {...pageProps} />
       </Layout>
     </ApolloProvider>
