c80f4cfbd19430887395695f0cf682ffd5691a91
Librariann <okpc0306@naver.com>
2024-04-21 10:00:10 +0900

    [commit] edit index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,8 @@
 "use client";
-
-import { authTokenVar, client, isLoggedInVar } from "@/apollo";
-import { LOCAL_STORAGE_TOKEN } from "@/common/constants";
-import { useRouter } from "next/router";
-
-export const clearToken = () => {
-  localStorage.setItem(LOCAL_STORAGE_TOKEN, "");
-  authTokenVar(null);
-  isLoggedInVar(false);
-  client.resetStore(); // Apollo Client 캐시 초기화
-};
-
 export default function Home() {
-  const router = useRouter();
-
-  const logout = () => {
-    clearToken();
-    router.push("/login");
-  };
-
-  const myPage = () => {
-    router.push("/my-page");
-  };
-
   return (
     <>
       <div className="">Hello World!</div>
-      <button onClick={myPage}>MyPage</button>
-      <button onClick={logout}>로그아웃</button>
     </>
   );
 }
