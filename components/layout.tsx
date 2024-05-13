34e6d78abdf2177ecda5ba1a115149bc6e924f3a
Librariann <okpc0306@naver.com>
2024-05-13 10:00:00 +0900

    [commit] edit layout.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,11 +1,21 @@
+import Header from "./header";
+import LeftNavigator from "./left-navigator";
+
 type Props = {
   children: React.ReactNode;
 };
-
+export const handlePathes = ["/login", "/create-account"];
 function Layout({ children }: Props) {
+  const { pathname } = useRouter();
+  const showHeader = !handlePathes.includes(pathname);
   return (
     <>
-      <div className="p-0 font-sans">{children}</div>
+      <div className="p-0 font-sans">
+        {/* <LoginCheck */}
+        {showHeader && <Header />}
+        {showHeader && <LeftNavigator />}
+        {children}
+      </div>
     </>
   );
 }
