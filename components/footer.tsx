5a55a407af79e80674860586d87b22f4026d7ca1
Librariann <okpc0306@naver.com>
2024-05-18 10:00:00 +0900

    [commit] add footer.tsx

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Footer = () => {
+  return (
+    <div className="w-full h-16 flex items-center bg-header-color pl-5">
+      <div className="flex ml-auto pr-5 text-white cursor-pointer">footer</div>
+    </div>
+  );
+};
+
+export default Footer;
