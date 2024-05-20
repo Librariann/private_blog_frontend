266d584dc59eb4e375933bd758b0f639326d0f86
Librariann <okpc0306@naver.com>
2024-05-20 10:00:00 +0900

    [commit] edit footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,9 +2,9 @@
 
 const Footer = () => {
   return (
-    <div className="w-full h-16 flex items-center bg-header-color pl-5">
+    <footer className="w-full h-16 flex items-center bg-header-color pl-5">
       <div className="flex ml-auto pr-5 text-white cursor-pointer">footer</div>
-    </div>
+    </footer>
   );
 };
 
