646c925a2ae7faf06f405e84a9f41df4e384a462
Librariann <okpc0306@naver.com>
2024-05-21 10:00:00 +0900

    [commit] edit left-navigator.tsx

--- components/left-navigator.tsx
+++ components/left-navigator.tsx
@@ -1,14 +1,47 @@
 "use client";
 
+import {
+  GetCategoriesCountsQuery,
+  GetCategoriesQuery,
+  GetCategoriesQueryVariables,
+} from "@/pages/gql/graphql";
+import { gql, useQuery } from "@apollo/client";
+
+export const GET_CATEGORIES_COUNTS_QUERY = gql`
+  query getCategoriesCounts {
+    getCategoriesCounts {
+      ok
+      categoryCounts {
+        categoryTitle
+        count
+      }
+    }
+  }
+`;
+
 const LeftNavigator = () => {
+  const { loading, error, data } = useQuery<
+    GetCategoriesCountsQuery,
+    GetCategoriesQueryVariables
+  >(GET_CATEGORIES_COUNTS_QUERY);
   return (
-    <nav>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-      </ul>
+    <nav className="h-full bg-gray-800 text-white p-4">
+      {!loading && (
+        <ul className="space-y-2">
+          {data?.getCategoriesCounts.categoryCounts?.map(
+            (categories, index) => (
+              <li key={index}>
+                <a
+                  href="#"
+                  className="block px-4 py-2 rounded hover:bg-gray-700"
+                >
+                  {categories.categoryTitle} ({categories.count})
+                </a>
+              </li>
+            )
+          )}
+        </ul>
+      )}
     </nav>
   );
 };
