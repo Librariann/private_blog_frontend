50c92cef6d33149d284b57c7999f5b8bbf26761c
Librariann <okpc0306@naver.com>
2024-05-21 10:00:00 +0900

    [commit] edit layout.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,8 @@
+import { useRouter } from "next/router";
 import Header from "./header";
 import LeftNavigator from "./left-navigator";
+import Footer from "./footer";
+import Contents from "./contents";
 
 type Props = {
   children: React.ReactNode;
@@ -7,14 +10,26 @@ type Props = {
 export const handlePathes = ["/login", "/create-account"];
 function Layout({ children }: Props) {
   const { pathname } = useRouter();
-  const showHeader = !handlePathes.includes(pathname);
+  const isLayoutVisible = !handlePathes.includes(pathname);
   return (
     <>
-      <div className="p-0 font-sans">
+      <div className="p-0 font-sans flex flex-col min-h-screen">
         {/* <LoginCheck */}
-        {showHeader && <Header />}
-        {showHeader && <LeftNavigator />}
+        {isLayoutVisible && <Header />}
+        {isLayoutVisible && (
+          <div className="flex-grow" style={{ height: "calc(100vh - 128px)" }}>
+            <div className="flex flex-row h-full">
+              <div className="w-1/6">
+                <LeftNavigator />
+              </div>
+              <div className="w-full">
+                <Contents />
+              </div>
+            </div>
+          </div>
+        )}
         {children}
+        {isLayoutVisible && <Footer />}
       </div>
     </>
   );
