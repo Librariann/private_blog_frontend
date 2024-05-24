bb38c2a193a173a25b0161736eaff0ae0d6601a8
Librariann <okpc0306@naver.com>
2024-05-24 10:00:00 +0900

    [commit] edit _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,25 +5,12 @@ import Layout from "../components/layout";
 import "../styles/globals.css";
 import { client } from "@/apollo";
 import { ApolloProvider } from "@apollo/client";
-import LoginCheck from "../components/login-check";
-import Header from "@/components/header";
-import { useRouter } from "next/router";
-import LeftNavigator from "@/components/left-navigator";
-import Footer from "@/components/footer";
-
-export const handlePathes = ["/login", "/create-account"];
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const { pathname } = useRouter();
-  const showHeader = !handlePathes.includes(pathname);
   return (
     <ApolloProvider client={client}>
       <Layout>
-        {/* <LoginCheck */}
-        {showHeader && <Header />}
-        {showHeader && <LeftNavigator />}
         <Component {...pageProps} />
-        <Footer />
       </Layout>
     </ApolloProvider>
   );
