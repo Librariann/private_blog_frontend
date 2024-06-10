11bb4051ec98f7451077f499885550d1f2060e6e
Librariann <okpc0306@naver.com>
2024-06-10 10:00:00 +0900

    [commit] edit layout.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,36 +2,42 @@ import { useRouter } from "next/router";
 import Header from "./header";
 import LeftNavigator from "./left-navigator";
 import Footer from "./footer";
-import Contents from "./contents";
+import { useState } from "react";
 
 type Props = {
   children: React.ReactNode;
 };
-export const handlePathes = ["/login", "/create-account"];
+export const handlePathes = ["/login", "/create-account", "/404"];
 function Layout({ children }: Props) {
+  const [selectedItem, setSelectedItem] = useState<string | null>(null);
   const { pathname } = useRouter();
   const isLayoutVisible = !handlePathes.includes(pathname);
+
+  const handleSelect = (item: string) => {
+    setSelectedItem(item);
+  };
+
+  console.log(selectedItem);
+
   return (
-    <>
-      <div className="p-0 font-sans flex flex-col min-h-screen">
-        {/* <LoginCheck */}
-        {isLayoutVisible && <Header />}
-        {isLayoutVisible && (
+    <div className="p-0 font-sans flex flex-col min-h-screen">
+      {isLayoutVisible ? (
+        <>
+          <Header />
           <div className="flex-grow" style={{ height: "calc(100vh - 128px)" }}>
             <div className="flex flex-row h-full">
               <div className="w-1/6">
-                <LeftNavigator />
+                <LeftNavigator selectItem={handleSelect} />
               </div>
-              <div className="w-full">
-                <Contents />
-              </div>
+              <div className="w-full">{children}</div>
             </div>
           </div>
-        )}
-        {children}
-        {isLayoutVisible && <Footer />}
-      </div>
-    </>
+          <Footer />
+        </>
+      ) : (
+        <div className="w-full">{children}</div>
+      )}
+    </div>
   );
 }
 
