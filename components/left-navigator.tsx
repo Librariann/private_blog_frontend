9c56666e502b2e75a2699d962af37c8355f915ff
Librariann <okpc0306@naver.com>
2024-06-11 10:00:00 +0900

    [commit] edit left-navigator.tsx

--- components/left-navigator.tsx
+++ components/left-navigator.tsx
@@ -5,12 +5,14 @@ import {
   GetCategoriesCountsQueryVariables,
 } from "@/pages/gql/graphql";
 import { gql, useQuery } from "@apollo/client";
+import Link from "next/link";
 
 export const GET_CATEGORIES_COUNTS_QUERY = gql`
   query getCategoriesCounts {
     getCategoriesCounts {
       ok
       categoryCounts {
+        id
         categoryTitle
         count
       }
@@ -18,7 +20,11 @@ export const GET_CATEGORIES_COUNTS_QUERY = gql`
   }
 `;
 
-const LeftNavigator = () => {
+type LeftNavigatorProps = {
+  selectItem: (item: string) => void;
+};
+
+const LeftNavigator = ({ selectItem }: LeftNavigatorProps) => {
   const { loading, data } = useQuery<
     GetCategoriesCountsQuery,
     GetCategoriesCountsQueryVariables
@@ -27,18 +33,21 @@ const LeftNavigator = () => {
     <nav className="h-full bg-gray-800 text-white p-4">
       {!loading && (
         <ul className="space-y-2">
-          {data?.getCategoriesCounts.categoryCounts?.map(
-            (categories, index) => (
-              <li key={index}>
-                <a
-                  href="#"
-                  className="block px-4 py-2 rounded hover:bg-gray-700"
-                >
-                  {categories.categoryTitle} ({categories.count})
-                </a>
-              </li>
-            )
-          )}
+          {data?.getCategoriesCounts.categoryCounts?.map((categories) => (
+            <li
+              key={categories.categoryTitle}
+              onClick={() => selectItem(categories.categoryTitle)}
+            >
+              <Link
+                href={{
+                  pathname: `/${encodeURIComponent(categories.categoryTitle)}`,
+                }}
+                className="block px-4 py-2 rounded hover:bg-gray-700"
+              >
+                {categories.categoryTitle} ({categories.count})
+              </Link>
+            </li>
+          ))}
         </ul>
       )}
     </nav>
