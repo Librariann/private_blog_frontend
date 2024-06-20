e66cb15a79f4519b8c938c8dc83651d435d0c237
Librariann <okpc0306@naver.com>
2024-06-20 10:00:00 +0900

    [commit] edit contents id index.tsx

--- pages/[contents]/[id]/index.tsx
+++ pages/[contents]/[id]/index.tsx
@@ -1,5 +1,76 @@
-function Post() {
-  return <div>Post!</div>;
-}
+import { GetServerSideProps } from "next";
+import { client } from "@/apollo"; // Apollo Client 설정 파일
+import { gql } from "@apollo/client";
+import {
+  GetPostByIdQuery,
+  GetPostByIdQueryVariables,
+} from "@/pages/gql/graphql";
+
+type PostProps = {
+  post: {
+    id: number;
+    title: string;
+    contents: string;
+    hits: number;
+  } | null;
+};
+
+export const GET_POST_BY_ID_QUERY = gql`
+  query getPostById($postId: Int!) {
+    getPostById(postId: $postId) {
+      ok
+      post {
+        id
+        title
+        contents
+        hits
+      }
+    }
+  }
+`;
+
+const Post = ({ post }: PostProps) => {
+  if (!post) {
+    return <div>Post not found!</div>;
+  }
+
+  return (
+    <div>
+      <h1>{post.title}</h1>
+      <h1>{post.contents}</h1>
+      <h1>{post.hits}</h1>
+    </div>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { params } = context;
+  const postId = Number(params?.id); // URL에서 postId를 추출합니다.
+
+  try {
+    const { data } = await client.query<
+      GetPostByIdQuery,
+      GetPostByIdQueryVariables
+    >({
+      query: GET_POST_BY_ID_QUERY,
+      variables: { postId },
+    });
+    if (!data.getPostById?.ok) {
+      return {
+        notFound: true, // 포스트가 없으면 404 페이지로 리디렉션합니다.
+      };
+    }
+
+    return {
+      props: {
+        post: data.getPostById.post,
+      },
+    };
+  } catch (error) {
+    return {
+      notFound: true, // 에러 발생 시 404 페이지로 리디렉션
+    };
+  }
+};
 
 export default Post;
