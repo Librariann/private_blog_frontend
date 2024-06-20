52c848eecfe6b82fdd64d4266c154754a71efcb1
Librariann <okpc0306@naver.com>
2024-06-20 10:00:00 +0900

    [commit] edit left-navigator.tsx

--- components/left-navigator.tsx
+++ components/left-navigator.tsx
@@ -5,7 +5,8 @@ import {
   GetCategoriesCountsQueryVariables,
 } from "@/pages/gql/graphql";
 import { gql, useQuery } from "@apollo/client";
-import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 export const GET_CATEGORIES_COUNTS_QUERY = gql`
   query getCategoriesCounts {
@@ -29,23 +30,35 @@ const LeftNavigator = ({ selectItem }: LeftNavigatorProps) => {
     GetCategoriesCountsQuery,
     GetCategoriesCountsQueryVariables
   >(GET_CATEGORIES_COUNTS_QUERY);
+  const router = useRouter();
+  const [hydrated, setHydrated] = useState(false);
+
+  useEffect(() => {
+    setHydrated(true); // 컴포넌트가 클라이언트에서 렌더링된 후 hydrated 상태를 true로 설정
+  }, []);
+
+  if (!hydrated) {
+    // 클라이언트에서 렌더링되기 전에는 빈 상태로 유지하여 서버와 클라이언트의 HTML 불일치를 방지
+    return null;
+  }
+
+  const handleClick = (categoryTitle: string) => {
+    selectItem(categoryTitle);
+    router.push(`/${encodeURIComponent(categoryTitle)}`);
+  };
   return (
     <nav className="h-full bg-gray-800 text-white p-4">
-      {!loading && (
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
         <ul className="space-y-2">
           {data?.getCategoriesCounts.categoryCounts?.map((categories) => (
             <li
               key={categories.categoryTitle}
-              onClick={() => selectItem(categories.categoryTitle)}
+              onClick={() => handleClick(categories.categoryTitle)}
+              className="block px-4 py-2 rounded hover:bg-gray-700 cursor-pointer"
             >
-              <Link
-                href={{
-                  pathname: `/${encodeURIComponent(categories.categoryTitle)}`,
-                }}
-                className="block px-4 py-2 rounded hover:bg-gray-700"
-              >
-                {categories.categoryTitle} ({categories.count})
-              </Link>
+              {categories.categoryTitle} ({categories.count})
             </li>
           ))}
         </ul>
