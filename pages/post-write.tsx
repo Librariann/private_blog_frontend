bdb068d58011c1eb4f8879ec1aa6c7a6388f37b9
Librariann <okpc0306@naver.com>
2024-07-24 10:00:10 +0900

    [commit] edit post-write.tsx

--- pages/post-write.tsx
+++ pages/post-write.tsx
@@ -1,5 +1,46 @@
+"use client";
+import "@uiw/react-md-editor/markdown-editor.css";
+import "@uiw/react-markdown-preview/markdown.css";
+import dynamic from "next/dynamic";
+import { useState } from "react";
+import Button from "@/components/button";
+import { useForm } from "react-hook-form";
+
+type postingProps = {
+  posting: string;
+};
+
+const MDEditor = dynamic(() => import("@uiw/react-md-editor"), {
+  ssr: false,
+});
+
 function PostWrite() {
-  return <div>PostWrite!</div>;
+  const {
+    register,
+    getValues,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<postingProps>({
+    mode: "onChange",
+  });
+
+  const onSubmit = () => {
+    console.log(md);
+  };
+
+  const [md, setMd] = useState<string | undefined>("");
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <MDEditor value={md} onChange={setMd} autoFocus={true} height={800} />
+        <Button
+          canClick={isValid}
+          loading={false}
+          actionText="게시물 생성"
+        ></Button>
+      </form>
+    </div>
+  );
 }
 
 export default PostWrite;
