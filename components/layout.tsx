e1d46d21416ef23f50c7d0613bf0c6750b3a05df
Librariann <okpc0306@naver.com>
2024-08-14 10:00:10 +0900

    [edit] layout.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,21 +1,28 @@
 import { useRouter } from "next/router";
 import Header from "./header";
 import LeftNavigator from "./left-navigator";
-import { useState } from "react";
+import PostWriteButton from "./post-write-button";
+import { isLoggedInVar } from "@/apollo";
+import { useEffect } from "react";
 
 type Props = {
   children: React.ReactNode;
 };
 export const handlePathes = ["/login", "/create-account", "/404"];
+const authPage = ["/my-page", "/post-write"];
 function Layout({ children }: Props) {
-  const [selectedItem, setSelectedItem] = useState<string | null>(null);
-  const { pathname } = useRouter();
-  const isLayoutVisible = !handlePathes.includes(pathname);
+  const { pathname, push } = useRouter();
 
-  const handleSelect = (item: string) => {
-    setSelectedItem(item);
-  };
+  //로그인 했을경우에만 접근 가능
+  useEffect(() => {
+    // 클라이언트 측에서만 실행되도록 설정
+    if (authPage.includes(pathname) && !isLoggedInVar()) {
+      alert("권한이 없습니다.");
+      push("/");
+    }
+  });
 
+  const isLayoutVisible = !handlePathes.includes(pathname); //레이아웃 보여줘야할때
   return (
     <div className="p-0 font-sans flex flex-col min-h-screen">
       {isLayoutVisible ? (
@@ -24,11 +31,13 @@ function Layout({ children }: Props) {
           <div className="flex-grow" style={{ height: "calc(100vh - 64px)" }}>
             <div className="flex flex-row h-full">
               <div className="w-1/6">
-                <LeftNavigator selectItem={handleSelect} />
+                <LeftNavigator />
               </div>
               <div className="w-full overflow-y-auto">{children}</div>
             </div>
+            <PostWriteButton />
           </div>
+
           {/* <Footer /> */}
         </>
       ) : (
