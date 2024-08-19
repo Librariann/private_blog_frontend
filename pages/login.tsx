ddea09fa482416919fb0b3ca67d52c7471c52025
Librariann <okpc0306@naver.com>
2024-08-19 10:00:10 +0900

    [edit] login.tsx

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,7 +3,7 @@
 import { useForm } from "react-hook-form";
 import FormError from "@/components/form-error";
 import { gql, useMutation } from "@apollo/client";
-import { LoginMutation, LoginMutationVariables } from "./gql/graphql";
+import { LoginMutation, LoginMutationVariables } from "../src/gql/graphql";
 import Button from "@/components/button";
 import { useRouter } from "next/navigation";
 import { LOCAL_STORAGE_TOKEN } from "@/common/constants";
