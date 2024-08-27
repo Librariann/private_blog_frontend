7493c0590588c880165b270d9b520db57e84ca25
Librariann <okpc0306@naver.com>
2024-08-27 10:00:10 +0900

    [edit] _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import Layout from "../components/layout";
 import "../styles/globals.css";
 import { client } from "@/apollo";
 import { ApolloProvider } from "@apollo/client";
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ApolloProvider client={client}>
