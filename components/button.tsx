4cc9f785a06551657e560446edd594888689b6c7
Librariann <okpc0306@naver.com>
2024-09-04 10:00:10 +0900

    [edit] button css

--- components/button.tsx
+++ components/button.tsx
@@ -10,7 +10,7 @@ const Button: React.FC<IButtonProps> = ({ canClick, loading, actionText }) => {
   return (
     <button
       data-testid="button"
-      className={`px-6 bg-blue-500 rounded-md hover:bg-blue-600 text-lg font-medium focus:outline-none text-white py-2 transition-colors ${
+      className={`w-full md:w-auto py-3 text-base md:text-lg px-6 bg-blue-500 rounded-md hover:bg-blue-600 text-lg font-medium focus:outline-none text-white py-2 transition-colors ${
         canClick
           ? "bg-blue-500 hover:bg-blue-700"
           : "bg-gray-300 pointer-events-none"
