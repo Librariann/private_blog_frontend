c4e0741fe7d8ffcfd312a7359e11de235742684a
Librariann <okpc0306@naver.com>
2024-09-26 10:00:10 +0900

    [add] confirm-modal.tsx

--- components/modal/confirm-modal.tsx
+++ components/modal/confirm-modal.tsx
@@ -28,7 +28,9 @@ const ConfirmModal = ({
             취소
           </button>
           <button
-            onClick={onConfirm}
+            onClick={() => {
+              onConfirm();
+            }}
             className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors"
           >
             확인
