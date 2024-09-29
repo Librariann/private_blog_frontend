8abbb5b0b9a1f92ccaf42a15b9f011ac3ff9e51c
Librariann <okpc0306@naver.com>
2024-09-29 10:00:10 +0900

    [edit] caching process

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 import type { AppProps } from "next/app";
 import Layout from "../components/layout";
 import "../styles/globals.css";
-import { client } from "@/apollo";
+import { client, createApolloClient } from "@/apollo";
 import { ApolloProvider } from "@apollo/client";
 function MyApp({ Component, pageProps }: AppProps) {
+  const apolloClient = createApolloClient(pageProps.initialApolloState);
   return (
     <ApolloProvider client={client}>
       <Layout>
