70353479c99d5f8d2a4446df1a9ced3806b922e0
Librariann <okpc0306@naver.com>
2024-10-10 10:00:10 +0900

    [edit] _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,13 @@ import Layout from "../components/layout";
 import "../styles/globals.css";
 import { client, createApolloClient } from "@/apollo";
 import { ApolloProvider } from "@apollo/client";
+import "@uiw/react-md-editor/markdown-editor.css";
+import "@uiw/react-markdown-preview/markdown.css";
+
 function MyApp({ Component, pageProps }: AppProps) {
   const apolloClient = createApolloClient(pageProps.initialApolloState);
   return (
-    <ApolloProvider client={client}>
+    <ApolloProvider client={apolloClient}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
