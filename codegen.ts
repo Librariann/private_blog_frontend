8610df1e32ed41c9b9b43983fd213f9a2a37da9b
Librariann <okpc0306@naver.com>
2024-12-21 10:00:00 +0900

    fix GraphQL schema environment variable in codegen configuration

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3003/graphql",
+  schema: process.env.NEXT_PUBLIC_GRAPHQL_URI || "http://localhost:3003/graphql",
   documents: ["pages/**/*.tsx", "components/**/*.tsx"],
   generates: {
     "./gql/": {
