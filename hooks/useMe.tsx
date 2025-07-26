45b186134438d7d209e7517928447301352b5c3c
Librariann <okpc0306@naver.com>
2025-07-26 10:00:00 +0900

    Apply cache-and-network strategy to user authentication hook

--- hooks/useMe.tsx
+++ hooks/useMe.tsx
@@ -15,7 +15,10 @@ export const ME_QUERY = gql`
 `;
 
 export const useMe = () => {
-  const { data, error } = useQuery(ME_QUERY);
+  const { data, error } = useQuery(ME_QUERY, {
+    fetchPolicy: "cache-and-network",
+    nextFetchPolicy: "cache-first",
+  });
   if (error?.message === "Token has expired") {
     return { data: "Token has expired" };
   }
