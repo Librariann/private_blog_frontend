9cdf288f8cccfe756d91e8c521d2e444c820ca0c
Librariann <okpc0306@naver.com>
2025-09-06 10:00:00 +0900

    configure favicon settings in app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,11 +6,44 @@ import { ApolloProvider } from "@apollo/client";
 import "@uiw/react-md-editor/markdown-editor.css";
 import "@uiw/react-markdown-preview/markdown.css";
 import "aos/dist/aos.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const apolloClient = createApolloClient(pageProps.initialApolloState);
   return (
     <ApolloProvider client={apolloClient}>
+      <Head>
+        <link rel="icon" href="/favicon-32x32.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="48x48"
+          href="/favicon-48x48.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="64x64"
+          href="/favicon-64x64.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="64x64"
+          href="/favicon-64x64.png"
+        />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
