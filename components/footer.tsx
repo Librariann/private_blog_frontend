ae2057634072429d36a673128276c13054221170
Librariann <okpc0306@naver.com>
2025-09-20 13:10:00 +0900

    remove unused components and old routing structure

--- components/footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="w-full h-16 flex items-center bg-header-color pl-5">
-      <div className="flex ml-auto pr-5 text-white cursor-pointer">footer</div>
-    </footer>
-  );
-};
-
-export default Footer;
