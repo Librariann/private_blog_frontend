8c0fd46eddb1d28f08b8691a92478eb2ead1445e
Librariann <okpc0306@naver.com>
2025-10-08 10:00:00 +0900

    replace toast library from sonner to react-toastify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
 import type { AppProps } from "next/app";
 import Layout from "../components/layout";
 import "../styles/globals.css";
-import { client, createApolloClient } from "@/apollo";
+import { createApolloClient } from "@/apollo";
 import { ApolloProvider } from "@apollo/client";
 import "@uiw/react-md-editor/markdown-editor.css";
 import "@uiw/react-markdown-preview/markdown.css";
 import "aos/dist/aos.css";
 import "../styles/edit.css";
 import Head from "next/head";
-import { Toaster } from "react-hot-toast";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const apolloClient = createApolloClient(pageProps.initialApolloState);
@@ -42,30 +43,17 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
         <link rel="apple-touch-icon" sizes="64x64" href="/favicon-64x64.png" />
       </Head>
-      <Toaster
-        position="top-center"
-        reverseOrder={false}
-        toastOptions={{
-          duration: 3000,
-          style: {
-            background: "#363636",
-            color: "#fff",
-          },
-          success: {
-            duration: 3000,
-            iconTheme: {
-              primary: "#4ade80",
-              secondary: "#fff",
-            },
-          },
-          error: {
-            duration: 3000,
-            iconTheme: {
-              primary: "#ef4444",
-              secondary: "#fff",
-            },
-          },
-        }}
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
       />
       <Layout>
         <Component {...pageProps} />
