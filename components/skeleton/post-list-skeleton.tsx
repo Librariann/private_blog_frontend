57b40983b838411378e07bd88980b36e5990e405
Librariann <okpc0306@naver.com>
2025-10-25 17:59:13 +0900

    Simplify post list skeleton component structure

--- components/skeleton/post-list-skeleton.tsx
+++ components/skeleton/post-list-skeleton.tsx
@@ -6,13 +6,11 @@ interface PostListSkeletonProps {
 
 const PostListSkeleton: React.FC<PostListSkeletonProps> = ({ count = 6 }) => {
   return (
-    <div className="p-10">
-      <ul className="flex flex-wrap justify-start">
-        {[...Array(count)].map((_, index) => (
-          <PostSkeleton key={index} />
-        ))}
-      </ul>
-    </div>
+    <>
+      {[...Array(count)].map((_, index) => (
+        <PostSkeleton key={index} />
+      ))}
+    </>
   );
 };
 
