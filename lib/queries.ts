04a0cb8822264d0eb2d06348d88466e220c20b39
Librariann <okpc0306@naver.com>
2026-09-22 01:45:43 +0900

    bug fix show the summary

--- lib/queries.ts
+++ lib/queries.ts
@@ -9,6 +9,7 @@ export const GET_POST_BY_CATEGORYID_QUERY = gql`
       posts {
         id
         title
+        excerpt
         contents
         hits
         thumbnailUrl
@@ -41,6 +42,7 @@ export const GET_POST_BY_PARENT_CATEGORY_ID_QUERY = gql`
       posts {
         id
         title
+        excerpt
         contents
         hits
         thumbnailUrl
